ae54da613253511bf644d07d41d2078e212bdaf9
kweozz <r0771028@student.thomasmore.be>
2024-10-04 12:51:55 +0200

    added user

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -4,11 +4,19 @@ const router = express.Router();
 //get /api/v1/messages
 
 router.get("/", (req, res, next) => {
-    res.json({
+    res.status(200).json(
+    {
+
         message: "GET messages",
         status: "succes",
         data: {
-            "messages": {}
+            "messages": [
+                {
+                id: 1,
+                user:"john",
+                message:"Hello World!"
+            }
+            ]
         }
     })
 })

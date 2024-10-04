bf6d0652a867f5cb1218c7430f35b162058917ca
kweozz <r0771028@student.thomasmore.be>
2024-10-04 14:38:44 +0200

    deleted routes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/routes/api/v1/messages', require('./routes/api/v1/messages'));
+app.use('/api/v1/messages', require('./routes/api/v1/messages'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
